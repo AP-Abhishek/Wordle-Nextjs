f70924e983df61bb848c0687c04137a226a2e157
AP-Abhishek <abhishekap12005@gmail.com>
2026-01-07 20:26:09 +0530

    style: cleanup

--- app/api/target.ts
+++ app/api/target.ts
@@ -2,7 +2,6 @@ const API_URL: string = "https://random-word-api.herokuapp.com/word?length=5";
 
 export async function fetchTarget() {
     const res = await fetch(API_URL);
-    console.log(res)
     const data = await res.json();
     return data[0];
 }
--- app/components/WordLine.tsx
+++ app/components/WordLine.tsx
@@ -1,16 +1,15 @@
 "use client";
 
-
-interface WordLineProps {
+type WordLineProps = {
   guess: string | null;
   word_length: number;
   target: string;
   check_color: boolean;
-}
+};
 
 export default function WordLine({ guess, word_length, target, check_color }: WordLineProps) {
 
-  const letters = Array.from({length: word_length}, (_, i) => guess?.charAt(i) || "");
+  const letters = Array.from({ length: word_length }, (_, i) => guess?.charAt(i) || "");
 
   return (
     <div className="flex">
@@ -30,9 +29,9 @@ export default function WordLine({ guess, word_length, target, check_color }: Wo
             <div key={idx} className={`m-1 h-16 w-16 flex items-center justify-center text-2xl font-semibold capitalize border border-stone-900 ${classes}`}>
               {letter}
             </div>
-          )
+          );
         })
       }
     </div>
-  )
+  );
 }
